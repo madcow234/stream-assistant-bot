3cbdea6a8d023526eda9f8ceb29ef52360131571
MadCow234 <MadCow234@gmail.com>
2020-07-13 00:22:12 -0500

    Remove retrospective role when user leaves voice channel

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,5 +1,7 @@
-import { VoiceState } from 'discord.js'
-import log            from 'winston'
+import { CHANNEL, ROLE } from '../enums'
+import { removeRole }    from '../services/roleService'
+import { VoiceState }    from 'discord.js'
+import log               from 'winston'
 
 /**
  * Emitted whenever a member changes voice state - e.g. joins/leaves a channel, mutes/unmutes.
@@ -12,6 +14,9 @@ exports.run = async (oldState, newState) => {
     try {
         log.debug(`Received event 'voiceStateUpdate'.`)
 
+        if (oldState.channel && oldState.channel.name === CHANNEL.VOICE.RETROSPECTIVE.NAME) {
+            await removeRole(oldState.guild, oldState.member.user, ROLE.RETROSPECTIVE.NAME)
+        }
 
     } catch (err) {
         log.error(`[/events/voiceStateUpdate#run] ${err}`)
